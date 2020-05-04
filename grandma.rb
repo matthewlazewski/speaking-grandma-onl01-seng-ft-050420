2f75d7f6d4a41d77d1f9818416c1a33572af9b51
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-04 17:10:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
 def speak_to_grandma(str)
   if str == 'I LOVE YOU GRANDMA!'
     puts 'I LOVE YOU TOO PUMPKIN!'
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
